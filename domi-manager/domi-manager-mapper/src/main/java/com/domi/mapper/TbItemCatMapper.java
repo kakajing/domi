c9cc9e9d4c4367e24585c523987978e551abc606
jing <985812572@qq.com>
2016-11-16 19:54:24 +0800

    mapper文件

---
+++ domi-manager/domi-manager-mapper/src/main/java/com/domi/mapper/TbItemCatMapper.java
@@ -0,0 +1,32 @@
+package com.domi.mapper;
+
+import com.domi.pojo.TbItemCat;
+import com.domi.pojo.TbItemCatExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+
+public interface TbItemCatMapper {
+    int countByExample(TbItemCatExample example);
+
+    int deleteByExample(TbItemCatExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbItemCat record);
+
+    int insertSelective(TbItemCat record);
+
+    List<TbItemCat> selectByExample(TbItemCatExample example);
+
+    TbItemCat selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbItemCat record, @Param("example") TbItemCatExample example);
+
+    int updateByExample(@Param("record") TbItemCat record, @Param("example") TbItemCatExample example);
+
+    int updateByPrimaryKeySelective(TbItemCat record);
+
+    int updateByPrimaryKey(TbItemCat record);
+}
