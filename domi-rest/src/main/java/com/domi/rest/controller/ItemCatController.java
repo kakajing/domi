1b8f4e3120a9b14f992a29c4db5c3721e89456a4
jing <985812572@qq.com>
2016-11-23 21:00:25 +0800

    支持jsonp的第二种方法

--- domi-rest/src/main/java/com/domi/rest/controller/ItemCatController.java
+++ domi-rest/src/main/java/com/domi/rest/controller/ItemCatController.java
@@ -2,10 +2,9 @@ package com.domi.rest.controller;
 
 import com.domi.rest.pojo.ItemCatResult;
 import com.domi.rest.service.ItemCatService;
-import com.domi.utils.JsonUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,18 +20,32 @@ public class ItemCatController {
     @Autowired
     private ItemCatService itemCatService;
 
-    @RequestMapping(value = "/list",produces = MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
+//    @RequestMapping(value = "/list",produces = MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
+//    @ResponseBody
+//    public String genItemCatList(String callback){
+//        ItemCatResult result = itemCatService.getItemCatList();
+//        if (StringUtils.isBlank(callback)){
+//            //需要把result转换成字符串
+//            String json = JsonUtils.objectToJson(result);
+//            return json;
+//        }
+//        //如果字符串不为空，需要支持jsonp调用
+//        //需要把result转换成字符串
+//        String json = JsonUtils.objectToJson(result);
+//        return callback + "(" + json + ");";
+//    }
+
+    @RequestMapping("/list")
     @ResponseBody
-    public String genItemCatList(String callback){
+    public Object genItemCatList(String callback){
         ItemCatResult result = itemCatService.getItemCatList();
         if (StringUtils.isBlank(callback)){
             //需要把result转换成字符串
-            String json = JsonUtils.objectToJson(result);
-            return json;
+            return result;
         }
         //如果字符串不为空，需要支持jsonp调用
-        //需要把result转换成字符串
-        String json = JsonUtils.objectToJson(result);
-        return callback + "(" + json + ");";
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(result);
+        mappingJacksonValue.setJsonpFunction(callback);
+        return mappingJacksonValue;
     }
 }
